672ab4f2018aaf616f2d7be18d1a8ab43ac00604
jicool91 <jicool912@gmail.com>
2017-05-07 17:24:22 +0300

    mm

---
+++ Dmitriy Fevralev/lesson1.mm
@@ -0,0 +1,29 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1494165707828" ID="ID_138354406" MODIFIED="1494165707828" TEXT="&#x41d;&#x43e;&#x432;&#x430;&#x44f; &#x43a;&#x430;&#x440;&#x442;&#x430;">
+<node CREATED="1494165748784" HGAP="71" ID="ID_1047778096" MODIFIED="1494166171864" POSITION="right" TEXT="&#x41e;&#x43f;&#x435;&#x440;&#x430;&#x442;&#x43e;&#x440;&#x44b;" VSHIFT="65">
+<node CREATED="1494166121979" ID="ID_581420842" MODIFIED="1494166294501" TEXT="              + - * / %  = "/>
+<node CREATED="1494166177402" ID="ID_577306932" MODIFIED="1494166306524" TEXT="&gt;    &lt;   &lt;=    &gt;=      ==     ==="/>
+<node CREATED="1494166228083" ID="ID_938701773" MODIFIED="1494166283245" TEXT="                 !  - &#x41d;&#x415; "/>
+<node CREATED="1494166315351" ID="ID_288957431" MODIFIED="1494166333127" TEXT="                || - &#x418;&#x41b;&#x418;"/>
+<node CREATED="1494166336410" HGAP="21" ID="ID_1194598021" MODIFIED="1494166392459" TEXT="                  &amp;&amp; - &#x418;" VSHIFT="-24"/>
+<node CREATED="1494166393886" HGAP="34" ID="ID_1434400349" MODIFIED="1494166483727" TEXT="                 != !==" VSHIFT="-4"/>
+<node CREATED="1494166487883" ID="ID_1640589037" MODIFIED="1494166496320" TEXT=" ? - &#x442;&#x435;&#x440;&#x43d;&#x430;&#x440;&#x43d;&#x44b;&#x439; &#x43e;&#x43f;&#x435;&#x440;&#x430;&#x442;&#x43e;&#x440;"/>
+<node CREATED="1494166507032" ID="ID_1081360778" MODIFIED="1494166527795" TEXT=" + - ~ &#x443;&#x43d;&#x430;&#x440;&#x43d;&#x44b;&#x435; &#x43e;&#x43f;&#x435;&#x440;&#x430;&#x442;&#x43e;&#x440;&#x44b;"/>
+</node>
+<node CREATED="1494165754193" HGAP="16" ID="ID_1575986873" MODIFIED="1494166115855" POSITION="left" TEXT="&#x422;&#x438;&#x43f;&#x44b; &#x434;&#x430;&#x43d;&#x43d;&#x44b;&#x445;" VSHIFT="20">
+<node CREATED="1494165832852" ID="ID_1868172149" MODIFIED="1494166023653" TEXT="Number                 "/>
+<node CREATED="1494165869455" ID="ID_336778382" MODIFIED="1494166045038" TEXT="String            &quot;&quot;  "/>
+<node CREATED="1494165877990" ID="ID_197377697" MODIFIED="1494166012265" TEXT="Boolean true/false"/>
+<node CREATED="1494165884687" ID="ID_1159805713" MODIFIED="1494166055965" TEXT="Array      []"/>
+<node CREATED="1494165911716" ID="ID_84071742" MODIFIED="1494166052169" TEXT="Object    {}"/>
+<node CREATED="1494165938326" ID="ID_633819869" MODIFIED="1494166059156" TEXT="null              "/>
+<node CREATED="1494165944021" ID="ID_424947383" MODIFIED="1494166063382" TEXT="undefined            "/>
+</node>
+<node CREATED="1494166558375" ID="ID_1210460414" MODIFIED="1494166570983" POSITION="right" TEXT="&#x41c;&#x435;&#x442;&#x43e;&#x434;&#x44b;">
+<node CREATED="1494166576342" ID="ID_616639428" MODIFIED="1494166665177" TEXT="parseInt(...) - &#x43f;&#x440;&#x438;&#x432;&#x435;&#x434;&#x435;&#x43d;&#x438;&#x435; &#x43a; &#x446;&#x435;&#x43b;&#x43e;&#x43c;&#x443; &#x447;&#x438;&#x441;&#x43b;&#x443;"/>
+<node CREATED="1494166599495" ID="ID_1528799297" MODIFIED="1494166652397" TEXT="name.toFixed(N) - &#x43f;&#x440;&#x438;&#x432;&#x435;&#x434;&#x435;&#x43d;&#x438;&#x435; &#x43a; &#x447;&#x438;&#x441;&#x43b;&#x443; &#x441; N-&#x437;&#x43d;&#x430;&#x43a;&#x43e;&#x432; &#x43f;&#x43e;&#x441;&#x43b;&#x435; &#x437;&#x430;&#x43f;&#x44f;&#x442;&#x43e;&#x439;"/>
+<node CREATED="1494166670323" ID="ID_566880838" MODIFIED="1494166769027" TEXT="typeof(...) - &#x432;&#x43e;&#x437;&#x432;&#x440;&#x430;&#x449;&#x430;&#x435;&#x442; &#x442;&#x438;&#x43f; &#x434;&#x430;&#x43d;&#x43d;&#x44b;&#x445;"/>
+</node>
+</node>
+</map>
